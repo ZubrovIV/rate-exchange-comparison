5f13bce4d76437b171512989d2e53967e84a16ef
ZubrovIV <zubroviv@gmail.com>
2022-06-09 23:41:45 +0300

    creating a project with dependencies and docker file

---
+++ src/main/java/ru/zubrov/rateexchangecomparison/RateExchangeComparisonApplication.java
@@ -0,0 +1,13 @@
+package ru.zubrov.rateexchangecomparison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateExchangeComparisonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateExchangeComparisonApplication.class, args);
+	}
+
+}
